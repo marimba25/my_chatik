09de9f4a8424eaf2d7bcf1a9c28e12dfff955d3c
marina <inwaiting2010@gmail.com>
2017-12-09 21:40:26 +0300

    попытка собрать

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='my_chatik',
     packages=find_packages(),
-    package_data={'': ['*']},
+    package_data={'': ['*.py', '*.ui', '*.jpg', '*.png']},
     version='0.1.1'
 )
