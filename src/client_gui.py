c3b4da3e207d80d5f2ef44374fc52a426bc99cf0
marina <inwaiting2010@gmail.com>
2017-12-14 17:56:43 +0300

    масштабирование аватарок

--- src/client_gui.py
+++ src/client_gui.py
@@ -123,7 +123,7 @@ window.pushButtonHelp.clicked.connect(show_help)
 
 
 def draw_avatar(image):
-    pixmap = QPixmap.fromImage(image).scaled(120, 120)
+    pixmap = QPixmap.fromImage(image).scaled(140, 140)
     window.avatar.setPixmap(pixmap)
 
 
